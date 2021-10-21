c7e7a2f8912d8f832910234c172589d98cfda3aa
Dung Nguyen Hoang <dung.nguyenhoang@nfq.asia>
2021-10-21 15:53:33 +0700

    initial UI for scripting

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,23 +1,57 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
+import i18n from "@/i18n";
+import "@/sass/overrides.sass";
+
+Vuetify.config.silent = false;
 
 Vue.use(Vuetify);
 
-export default new Vuetify({
-  theme: {
-      options: {
-        customProperties: true,
-      },
-    themes: {
-      light: {
-        primary: '#007BFF',
-        secondary: '#424242',
-        accent: '#82B1FF',
-        error: '#FF5252',
-        info: '#2196F3',
-        success: '#4CAF50',
-        warning: '#FFC107'
-      },
+const theme = {
+  dark: false,
+  default: "light",
+  disable: false,
+  options: {
+    cspNonce: undefined,
+    customProperties: true,
+    minifyTheme: undefined,
+    themeCache: undefined,
+  },
+  themes: {
+    light: {
+      primary: "#1976D2",
+      secondary: "#424242",
+      accent: "#82B1FF",
+      error: "#FF5252",
+      info: "#2196F3",
+      success: "#4CAF50",
+      warning: "#FB8C00",
     },
+    dark: {
+      primary: "#2196F3",
+      secondary: "#424242",
+      accent: "#FF4081",
+      error: "#FF5252",
+      info: "#2196F3",
+      success: "#4CAF50",
+      warning: "#FB8C00",
+    },
+  },
+};
+const breakpoint = {
+  scrollBarWidth: 16,
+  thresholds: {
+    xs: 600,
+    sm: 960,
+    md: 1280,
+    lg: 1920,
+  },
+};
+
+export default new Vuetify({
+  theme,
+  breakpoint,
+  lang: {
+    t: (key, ...params) => i18n.t(key, params).toString(),
   },
 });
